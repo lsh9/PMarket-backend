95e549ac06abe3ef95124d56f4f34f6de8739ea7
李帅 <1900012471@pku.edu.cn>
2022-11-07 01:21:56 +0800

    fix: bugs when uploading images

--- app/api/image.py
+++ app/api/image.py
@@ -1,29 +1,29 @@
 from flask import Blueprint, request, Response, current_app
 import hashlib
-
+import base64
 image_bp = Blueprint("image_bp", __name__)
 
 
 @image_bp.route('/image/upload/<by>', methods=["POST"])
 def upload_img(by):
-	if by == 'avatar':
-		pass
-	elif by == 'goods':
-		pass
-	else:
-		return None
-	data = request.json
-	image_BYTE = data.get('image')
-	path = f"images/{by}/"
-	imageId = hashlib.md5(str(image_BYTE).encode('utf-8')).hexdigest()
-	with open(path + imageId, "wb") as f:
-		f.write(image_BYTE)
-	url = current_app.config['HTTP_SERVER'] + f"/image/{by}/{imageId}"
-	return url
+	try:
+		if by not in ['avatar', 'goods']:
+			return {'code': 1}
+
+		image_BYTE = base64.b64decode(request.form.get("image"))
+		path = f"images/{by}/"
+		imageId = hashlib.md5(str(image_BYTE).encode('utf-8')).hexdigest()
+		with open(path + imageId, "wb") as f:
+			f.write(image_BYTE)
+		url = current_app.config['HTTP_SERVER'] + f"/image/{by}/{imageId}"
+		return {'url': url}
+	except Exception as e:
+		print("ERROR:", e)
+		return {'code': 1}
 
 
-@image_bp.route('/image/<imageId>', methods=["GET"])
-def image(imageId):
-	with open("images/{}".format(imageId), "rb") as f:
+@image_bp.route('/image/<by>/<imageId>', methods=["GET"])
+def image(by, imageId):
+	with open("images/{}/{}".format(by, imageId), "rb") as f:
 		img = f.read()
 	return Response(img, mimetype="image")
