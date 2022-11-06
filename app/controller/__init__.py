51353ba293db24afed48e284716cef20e971b64b
李帅 <1900012471@pku.edu.cn>
2022-11-06 23:25:56 +0800

    refactor: 重构user部分，降低耦合

--- app/controller/__init__.py
+++ app/controller/__init__.py
@@ -1,3 +1,3 @@
 from app.controller.test import test_add, test_delete, test_query_keyword
-from app.controller.user import add_user, query_user, update_user
+from app.controller.user import add_user, query_user, update_user, query_userid_by_openid
 from app.controller.goods import add_goods, update_goods, query_goods_by_id_and_limit
