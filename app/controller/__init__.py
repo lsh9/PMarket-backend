9dface9ee4e8191c2a1821b1697c65936e3731b0
AsTonyshment <baotaoni@163.com>
2022-11-30 07:37:43 +0800

    Testing historical release & goods likes

--- app/controller/__init__.py
+++ app/controller/__init__.py
@@ -1,4 +1,4 @@
 from app.controller.test import test_add, test_delete, test_query_keyword
-from app.controller.user import add_user, query_user, update_user, query_userid_by_openid, insert_fake_users
+from app.controller.user import add_user, query_user, update_user, query_userid_by_openid, insert_fake_users, show_all_users
 from app.controller.goods import add_goods, update_goods, query_goods_by_id_and_limit
 from app.controller.goods import *
